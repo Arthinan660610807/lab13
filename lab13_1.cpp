054bb404afb2445591a61cffcd44a976fb6f7a5f
H <f@gmail.com>
2024-01-02 14:26:18 +0700

    lab13_1-complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,53 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double arr[], int size, double result[]) {
+    if (size <= 0) {
+        cerr << "Invalid array size." << endl;
+        return;
+    }
+
+
+    double sum = 0.0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    result[0] = sum / size;
+
+   
+    double mean = result[0];
+    double sumSquaredDiff = 0.0;
+    for (int i = 0; i < size; ++i) {
+        sumSquaredDiff += pow(arr[i] - mean, 2);
+    }
+    result[1] = sqrt(sumSquaredDiff / size);
+
+   
+    double product = 1.0;
+    for (int i = 0; i < size; ++i) {
+        product *= arr[i];
+    }
+    result[2] = pow(product, 1.0 / size);
+
+  
+    double reciprocalSum = 0.0;
+    for (int i = 0; i < size; ++i) {
+        reciprocalSum += 1.0 / arr[i];
+    }
+    result[3] = size / reciprocalSum;
+
+   
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    result[4] = max;
+    result[5] = min;
+}
